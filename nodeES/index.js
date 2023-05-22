6e2b07ec48c780fd9439bb244a5df433d1771a11
Jose Pacifico <alberto14.filho@gmail.com>
2023-05-21 22:59:25 -0400

    Exercicio VI - ES Modules

---
+++ nodeES/index.js
@@ -0,0 +1,35 @@
+import http from "http";
+import fs from "fs";
+import { createLink, createBack } from "./util.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3333;
+const folder = process.argv[2];
+console.log(folder);
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html; charset=utf-8",
+  });
+
+  fs.readdir(folder, (err, files) => {
+    if (req.url === "/") {
+      if (err) console.error(err);
+      else {
+        files.forEach((f) => res.write(`${createLink(f)}`));
+        res.end();
+      }
+    } else {
+      fs.readFile(`${folder}/${req.url}`, (err, content) => {
+        res.write(createBack());
+        res.end(content);
+      });
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Rodando na porta ${PORT}`);
+});
---
+++ nodeES/util.js
@@ -0,0 +1,7 @@
+export function createLink(filename) {
+    return `<a href="/${filename}">${filename}</a><br>\n`;
+}
+
+export function createBack() {
+    return `<a href='/'>VOLTAR</a><br>\n`;
+}
